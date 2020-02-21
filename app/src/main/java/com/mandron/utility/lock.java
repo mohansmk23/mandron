e866b49026402630c6befbe67184eeaf1046a779
MohanKumar S <mohankumar.istrides@gmail.com>
2020-02-21 19:22:12 +0530

    lock

--- app/src/main/java/com/mandron/utility/lock.java
+++ app/src/main/java/com/mandron/utility/lock.java
@@ -1,14 +1,60 @@
 package com.mandron.utility;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
 
 public class lock extends AppCompatActivity {
+
+
+    public static final String TAG = "PinLockView";
+    private PinLockView mPinLockView;
+    private IndicatorDots mIndicatorDots;
+
+    private PinLockListener mPinLockListener = new PinLockListener() {
+        @Override
+        public void onComplete(String pin) {
+            Log.d(TAG, "Pin complete: " + pin);
+        }
+
+        @Override
+        public void onEmpty() {
+            Log.d(TAG, "Pin empty");
+        }
+
+        @Override
+        public void onPinChange(int pinLength, String intermediatePin) {
+            Log.d(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_lock);
+
+        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
+        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
+        //mPinLockView.setCustomKeySet(new int[]{2, 3, 1, 5, 9, 6, 7, 0, 8, 4});
+        //mPinLockView.enableLayoutShuffling();
+
+        mPinLockView.setPinLength(4);
+        mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+        mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
     }
 }
